a5e075f683caf705131d6d27a11fa21a73992edd
José Lisboa <josemrlisboa@gmail.com>
2024-09-15 20:33:41 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,6 +26,8 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
+  access_key = "${{secrets.AWS_SECRET_ACCESS_KEY}}"
+  secret_key = "${{secrets.AWS_ACCESS_KEY_ID}}"
 }
 
 resource "random_pet" "sg" {}
