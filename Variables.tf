91a5a7ee84285a77bfd4f8708ebea4b9888f3951
ervinnotari <ervinnotari@hotmail.com>
2024-09-22 22:56:57 -0300

    action

--- Variables.tf
+++ Variables.tf
@@ -1,34 +1,34 @@
 variable "aws_region" {
-  type = string
-  default = "us-east-1"
+  type        = string
+  default     = "us-east-1"
   description = "Define a região padrão da AWS onde os recursos serão provisionados."
 }
 
 variable "project_name" {
-  type = string
-  default = "MechTechApi"
+  type        = string
+  default     = "MechTechApi"
   description = "Especifica o nome do projeto"
 }
 
 variable "instanceType" {
-  type = string
-  default = "t3a.medium"
+  type        = string
+  default     = "t3a.medium"
   description = "Especifica o tipo de instância EC2 que será utilizada, sendo uma instância de uso geral com equilíbrio entre capacidade de computação e custo."
 }
 
 variable "accountIdVoclabs" {
-  type = string
+  type        = string
   description = "Representa o ID da conta da AWS para o laboratório vocacional. Esta variável é obrigatória."
 }
 
 variable "policyArn" {
-  type = string
-  default = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  type        = string
+  default     = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
   description = "Define o Amazon Resource Name (ARN) de uma política específica que pode ser aplicada a recursos ou usuários. Esta variável é obrigatória."
 }
 
 variable "accessConfig" {
-  type = string
-  default = "API_AND_CONFIG_MAP"
+  type        = string
+  default     = "API_AND_CONFIG_MAP"
   description = "Especifica a configuração de acesso desejada, incluindo tanto a API quanto um mapa de configuração."
 }
