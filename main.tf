450accb51578a5f745feff30821c19bdfe979e88
ervinnotari <ervinnotari@hotmail.com>
2024-09-25 18:37:52 -0300

    action

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
-  backend "s3" {
-    bucket = "fiap-backend-tf"
+/*  backend "s3" {
+    bucket = "fiap-backend-tf-" + var.aws_account_id
     key    = "backend/terraform.tfstate"
     region = "us-east-1"
-  }
+  }*/
 }
