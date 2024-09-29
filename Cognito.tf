ea6435098fbd1694dd50d512d590791796e2401c
ervinnotari <ervinnotari@hotmail.com>
2024-09-29 03:06:57 -0300

    cognito configs

--- Cognito.tf
+++ Cognito.tf
@@ -38,7 +38,6 @@ resource "kubernetes_secret" "cognito_secrets" {
   data = {
     OPENID_AUTHORITY = "https://cognito-idp.${var.aws_region}.amazonaws.com/${aws_cognito_user_pool.mctech_cg.id}"
     OPENID_AUDIENCE  = aws_cognito_user_pool_client.mctech_cg_client.id
-    # OPENID_SECRET    = aws_cognito_user_pool_client.mctech_cg_client.client_secret
   }
 
   depends_on = [
